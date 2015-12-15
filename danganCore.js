08832feb5b0576cbc79bea4931f0015dcddbed58
Zehao Li <qft.gtr@gmail.com>
2015-12-15 16:30:49 +0800

    flower pos

--- danganCore.js
+++ danganCore.js
@@ -66,7 +66,7 @@ var DanganCore = (function(undefined) {
       d.value = '/static/images/print/template/'+(_star2[v] || 'star_0.png');
     },
     'emptyFlower': function(d) {
-      if (d.value)
+      if (!d.value)
         d.x = d.x+106;
       
       d.value = '/static/images/print/template/red%20flower.png';
