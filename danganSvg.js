ae4380cbae24cd65e6bf1fde4f3a21f7e1c7ac2c
Zehao Li <qft.gtr@gmail.com>
2015-12-19 12:41:33 +0800

    zoomed

--- danganSvg.js
+++ danganSvg.js
@@ -342,7 +342,7 @@ var DanganSVG = function() {
       _clone.init(id, {});
       
       size = size || {};
-      _clone.size(size.w||(_width/_zoom), size.h||(_height/_zoom), size.zoom||1);
+      _clone.size(size.w||(_width/_zoom), size.h||(_height/_zoom), size.zoom||_zoom);
       return _clone;
     },
     wrapText: wrapText
